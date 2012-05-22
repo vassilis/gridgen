48e31fb9470ddc0ca0f2aca6f21cd2b4d8168e7e
Vassilis <vas.terzo@gmail.com>
2012-05-22 16:07:41 +0300

    improved UI select elements and added code to collect custom id and class values

--- grid.coffee
+++ grid.coffee
@@ -9,13 +9,46 @@
 		$('#insert-before, #insert-after').hide()
 		$('#row-settings, #col-settings, #content-options').hide()
 
+	$.fn.hasAttr = (name) ->  
+		@attr(name) != undefined && @attr(name) != false
+
+	updateColWidth = (width) ->
+		elem = $('.selected.col')
+		w = eval(elem.attr('data-width'))
+		wnew = eval(width)
+		diff = eval(w - wnew)
+		group = []
+		elem.nextAll().each ->
+			group.push($(this))
+		elem.prevAll().each ->
+			group.push($(this))
+		if diff != 0
+			i = 1
+			while i <= Math.abs(diff)
+				s = ''
+				$(group).each (index) ->
+					if diff < 0
+						tw = eval($(this).attr('data-width'))
+						if tw > 1 
+							s = $(this)
+							return false
+					else
+						s = $(this)
+						return false
+				if s != ''
+					sw = eval(s.attr('data-width'))
+					swnew = if diff > 0 then eval(sw + 1) else eval(sw - 1) 
+					elem.removeClass('span_' + w).addClass('span_' + wnew).attr('data-width', wnew)
+					s.removeClass('span_' + sw).addClass('span_' + swnew).attr('data-width', swnew)
+				i++
+
 	$ ->
 
 		body = $('body')
 
 		hideSelectedTools()
 
-		$('#menu .button').not('#menu-container-settings').on 'click', ->
+		$('#menu .button, #logo').not('#menu-container-settings').on 'click', ->
 			$('.toolbar').hide()
 
 		$('#menu-container-settings').on 'click', ->
@@ -30,7 +63,7 @@
 			if ! $('#user-styles').length
 				style = $('<style id="user-styles"></style>')
 				style.appendTo('head')
-			$('#styles').val($('#user-styles').val())
+			$('#styles').val($('#user-styles').html())
 			$('#styles-panel').show()
 
 		$('#menu-editor').on 'click', ->
@@ -40,10 +73,45 @@
 		$("#container-width").on 'keyup', ->
 			$("#page").css "width", $(this).val()
 
+		$('.adjustable').each ->
+			elem = $(this)
+			if elem.hasAttr('data-values')
+				values = elem.attr('data-values').replace(/\ /g, '').split(',')
+				elem.text(values[0])
+			else
+				elem.text(0)
+
+		body.on 'click', '.sb-plus, .sb-minus', (e) ->
+			elem = $(this)
+			field = elem.siblings('.adjustable').first()
+			value = field.text()
+			if field.hasAttr('data-values')
+				values = field.attr('data-values').replace(/\ /g, '').split(',')
+				if value in values
+					if $(e.target).hasClass('sb-plus') and values.indexOf(value) < values.length - 1
+						field.text(values[values.indexOf(value) + 1])
+					else if $(e.target).hasClass('sb-minus') and values.indexOf(value) > 0
+						field.text(values[values.indexOf(value) - 1])
+				else
+					field.text(values[0])
+			else
+				if typeof value != 'undefined'
+					if $(e.target).hasClass('sb-plus') 
+						field.text(eval(value + 1))
+					else if $(e.target).hasClass('sb-minus')
+						if value > 0 or elem.hasClass('allow-negative')
+							field.text(eval(value - 1))
+				else
+					if $(e.target).hasClass('sb-plus') 
+						field.text(1)
+					else if $(e.target).hasClass('sb-minus') and elem.hasClass('allow-negative')
+						field.text(-1)
+
+
 		$('#insert-before, #insert-prepend, #insert-append, #insert-after').on 'click', (e) ->
 			o = $('#insert-position').data('o')
 			if o == 'grid'
-				columns = eval($("#grid-columns").val())
+				columns = eval($("#grid-columns").text())
 				# gutter = eval($("#grid-gutter").val())
 				row = $('<section class="row span_24"></section>')
 				i = 1
@@ -77,6 +145,9 @@
 				$('#menu-col-settings').click()
 			else if $('#row-settings').is(':visible')
 				$('#menu-row-settings').click()
+			else if $('#editor').is(':visible')
+				$('#menu-editor').click()
+			if elem.siblings().length then $('#col-width').parent().show() else $('#col-width').parent().hide()
 			elem.on 'clickoutside', (e) ->
 				unless $(e.target).hasClass('toolbar')\
 					or $(e.target).attr('id') == 'menu'\
@@ -85,70 +156,88 @@
 						unless $(e.target).hasClass('col')
 							hideSelectedTools()
 						elem.off 'clickoutside'
+			return false
 
 		$('#menu-row-settings').on 'click', ->
 			elem = $('.col.selected')
 			if elem.length
 				row = $('.col.selected').closest('.row')
 				$('#row-settings').show()
+				# id
+				$('#row-id').val(row.attr('id'))
+				# classes
+				$('#row-class').val($.trim(row.attr('class').replace(/row/g, '').replace(/span_\d*/g, '')))
 
 		$('#menu-col-settings').on 'click', ->
 			elem = $('.col.selected')
+			$('#col-settings').show()
+			# id
+			$('#col-id').val(elem.attr('id'))
+			# classes
+			$('#col-class').val($.trim(elem.attr('class').replace(/selected/g, '').replace(/col/g, '').replace(/span_\d*/g, '')))
+			# width
 			if elem.length and elem.siblings().length
 				width = elem.attr('data-width')
 				# $('.col.selected').removeClass 'selected'
 				# elem.addClass 'selected'
-				$('#col-settings').show()
 				i = 1
-				$('#col-width').html('')
+				$('#col-width').attr('data-values', '')
 				while i <= 24 - elem.siblings().length
-					$('#col-width').append '<option value="' + i + '">' + i + '</option>'
+					$('#col-width').attr('data-values', $('#col-width').attr('data-values') + i)
+					if i < 24 - elem.siblings().length then $('#col-width').attr('data-values', $('#col-width').attr('data-values') + ',')
 					i++
-				$('#col-width').val(width).focus()
+				$('#col-width').text(width)
 
 		$('#menu-content').on 'click', ->
 			elem = $('.col.selected')
 			if elem.length
 				$('#content-options').show()
 
-		$('#col-width').on 'change keyup', (e) ->
-			if $('.col.selected').length and e.which is 46
-				$('.col.selected').closest('.row').remove()
-				$('#col-settings').hide()
-			elem = $('.selected.col')
-			w = eval(elem.attr('data-width'))
-			wnew = eval($(this).val())
-			diff = eval(w - wnew)
-			group = []
-			elem.nextAll().each ->
-				group.push($(this))
-			elem.prevAll().each ->
-				group.push($(this))
-			if diff != 0
-				i = 1
-				while i <= Math.abs(diff)
-					s = ''
-					$(group).each (index) ->
-						if diff < 0
-							tw = eval($(this).attr('data-width'))
-							if tw > 1 
-								s = $(this)
-								return false
-						else
-							s = $(this)
-							return false
-					if s != ''
-						sw = eval(s.attr('data-width'))
-						swnew = if diff > 0 then eval(sw + 1) else eval(sw - 1) 
-						elem.removeClass('span_' + w).addClass('span_' + wnew).attr('data-width', wnew)
-						s.removeClass('span_' + sw).addClass('span_' + swnew).attr('data-width', swnew)
-					i++
+		$('#row-id').on 'keyup', ->
+			elem = $(this)
+			selectedcol = $('.col.selected')
+			if selectedcol.length
+				row = $('.col.selected').closest('.row')
+				row.attr('id', elem.val())
 
-		$('#styles').on 'keypress', ->
+		$('#row-class').on 'keyup', ->
+			elem = $(this)
+			selectedcol = $('.col.selected')
+			if selectedcol.length
+				row = $('.col.selected').closest('.row')
+				x1 = row.attr('class').match(/row/g)
+				x2 = row.attr('class').match(/span_\d*/g)
+				x = x1.concat(x2)
+				row.attr('class', x.toString().replace(/,/g, ' ') + ' ' + elem.val())
+
+		$('#col-id').on 'keyup', ->
+			elem = $(this)
+			selectedcol = $('.col.selected')
+			if selectedcol.length
+				selectedcol.attr('id', elem.val())
+
+		$('#col-class').on 'keyup', ->
+			elem = $(this)
+			selectedcol = $('.col.selected')
+			if selectedcol.length
+				x1 = selectedcol.attr('class').match(/col/g)
+				x2 = selectedcol.attr('class').match(/span_\d*/g)
+				x3 = selectedcol.attr('class').match(/selected/g)
+				x = x1.concat(x2, x3)
+				selectedcol.attr('class', x.toString().replace(/,/g, ' ') + ' ' + elem.val())
+
+		$('.col-width .sb').on 'click', ->
+			setTimeout(
+				->
+					updateColWidth($('#col-width').text())
+				, 10
+			)
+
+		$('#styles').on 'keyup', ->
 			style = $('#user-styles')
 			style.html($('#styles').val())
 
-		$('#editor').on 'keypress', ->
+		$('#editor').on 'keyup', ->
 			elem = $('.col.selected')
 			if elem.length
 				setTimeout(
--- grid.js
+++ grid.js
@@ -1,5 +1,6 @@
 (function() {
-  var hideSelectedTools, showSelectedTools;
+  var hideSelectedTools, showSelectedTools, updateColWidth,
+    __indexOf = Array.prototype.indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   showSelectedTools = function() {
     $('#menu-row-settings, #menu-col-settings, #menu-content').show();
@@ -12,11 +13,58 @@
     return $('#row-settings, #col-settings, #content-options').hide();
   };
 
+  $.fn.hasAttr = function(name) {
+    return this.attr(name) !== void 0 && this.attr(name) !== false;
+  };
+
+  updateColWidth = function(width) {
+    var diff, elem, group, i, s, sw, swnew, w, wnew, _results;
+    elem = $('.selected.col');
+    w = eval(elem.attr('data-width'));
+    wnew = eval(width);
+    diff = eval(w - wnew);
+    group = [];
+    elem.nextAll().each(function() {
+      return group.push($(this));
+    });
+    elem.prevAll().each(function() {
+      return group.push($(this));
+    });
+    if (diff !== 0) {
+      i = 1;
+      _results = [];
+      while (i <= Math.abs(diff)) {
+        s = '';
+        $(group).each(function(index) {
+          var tw;
+          if (diff < 0) {
+            tw = eval($(this).attr('data-width'));
+            if (tw > 1) {
+              s = $(this);
+              return false;
+            }
+          } else {
+            s = $(this);
+            return false;
+          }
+        });
+        if (s !== '') {
+          sw = eval(s.attr('data-width'));
+          swnew = diff > 0 ? eval(sw + 1) : eval(sw - 1);
+          elem.removeClass('span_' + w).addClass('span_' + wnew).attr('data-width', wnew);
+          s.removeClass('span_' + sw).addClass('span_' + swnew).attr('data-width', swnew);
+        }
+        _results.push(i++);
+      }
+      return _results;
+    }
+  };
+
   $(function() {
     var body;
     body = $('body');
     hideSelectedTools();
-    $('#menu .button').not('#menu-container-settings').on('click', function() {
+    $('#menu .button, #logo').not('#menu-container-settings').on('click', function() {
       return $('.toolbar').hide();
     });
     $('#menu-container-settings').on('click', function() {
@@ -33,7 +81,7 @@
         style = $('<style id="user-styles"></style>');
         style.appendTo('head');
       }
-      $('#styles').val($('#user-styles').val());
+      $('#styles').val($('#user-styles').html());
       return $('#styles-panel').show();
     });
     $('#menu-editor').on('click', function() {
@@ -43,11 +91,55 @@
     $("#container-width").on('keyup', function() {
       return $("#page").css("width", $(this).val());
     });
+    $('.adjustable').each(function() {
+      var elem, values;
+      elem = $(this);
+      if (elem.hasAttr('data-values')) {
+        values = elem.attr('data-values').replace(/\ /g, '').split(',');
+        return elem.text(values[0]);
+      } else {
+        return elem.text(0);
+      }
+    });
+    body.on('click', '.sb-plus, .sb-minus', function(e) {
+      var elem, field, value, values;
+      elem = $(this);
+      field = elem.siblings('.adjustable').first();
+      value = field.text();
+      if (field.hasAttr('data-values')) {
+        values = field.attr('data-values').replace(/\ /g, '').split(',');
+        if (__indexOf.call(values, value) >= 0) {
+          if ($(e.target).hasClass('sb-plus') && values.indexOf(value) < values.length - 1) {
+            return field.text(values[values.indexOf(value) + 1]);
+          } else if ($(e.target).hasClass('sb-minus') && values.indexOf(value) > 0) {
+            return field.text(values[values.indexOf(value) - 1]);
+          }
+        } else {
+          return field.text(values[0]);
+        }
+      } else {
+        if (typeof value !== 'undefined') {
+          if ($(e.target).hasClass('sb-plus')) {
+            return field.text(eval(value + 1));
+          } else if ($(e.target).hasClass('sb-minus')) {
+            if (value > 0 || elem.hasClass('allow-negative')) {
+              return field.text(eval(value - 1));
+            }
+          }
+        } else {
+          if ($(e.target).hasClass('sb-plus')) {
+            return field.text(1);
+          } else if ($(e.target).hasClass('sb-minus') && elem.hasClass('allow-negative')) {
+            return field.text(-1);
+          }
+        }
+      }
+    });
     $('#insert-before, #insert-prepend, #insert-append, #insert-after').on('click', function(e) {
       var col, columns, i, o, row, selectedcol, selectedrow, w;
       o = $('#insert-position').data('o');
       if (o === 'grid') {
-        columns = eval($("#grid-columns").val());
+        columns = eval($("#grid-columns").text());
         row = $('<section class="row span_24"></section>');
         i = 1;
         while (i <= columns) {
@@ -85,36 +177,51 @@
         $('#menu-col-settings').click();
       } else if ($('#row-settings').is(':visible')) {
         $('#menu-row-settings').click();
+      } else if ($('#editor').is(':visible')) {
+        $('#menu-editor').click();
       }
-      return elem.on('clickoutside', function(e) {
+      if (elem.siblings().length) {
+        $('#col-width').parent().show();
+      } else {
+        $('#col-width').parent().hide();
+      }
+      elem.on('clickoutside', function(e) {
         if (!($(e.target).hasClass('toolbar') || $(e.target).attr('id') === 'menu' || $(e.target).parents('.toolbar, #menu').length)) {
           elem.removeClass('selected');
           if (!$(e.target).hasClass('col')) hideSelectedTools();
           return elem.off('clickoutside');
         }
       });
+      return false;
     });
     $('#menu-row-settings').on('click', function() {
       var elem, row;
       elem = $('.col.selected');
       if (elem.length) {
         row = $('.col.selected').closest('.row');
-        return $('#row-settings').show();
+        $('#row-settings').show();
+        $('#row-id').val(row.attr('id'));
+        return $('#row-class').val($.trim(row.attr('class').replace(/row/g, '').replace(/span_\d*/g, '')));
       }
     });
     $('#menu-col-settings').on('click', function() {
       var elem, i, width;
       elem = $('.col.selected');
+      $('#col-settings').show();
+      $('#col-id').val(elem.attr('id'));
+      $('#col-class').val($.trim(elem.attr('class').replace(/selected/g, '').replace(/col/g, '').replace(/span_\d*/g, '')));
       if (elem.length && elem.siblings().length) {
         width = elem.attr('data-width');
-        $('#col-settings').show();
         i = 1;
-        $('#col-width').html('');
+        $('#col-width').attr('data-values', '');
         while (i <= 24 - elem.siblings().length) {
-          $('#col-width').append('<option value="' + i + '">' + i + '</option>');
+          $('#col-width').attr('data-values', $('#col-width').attr('data-values') + i);
+          if (i < 24 - elem.siblings().length) {
+            $('#col-width').attr('data-values', $('#col-width').attr('data-values') + ',');
+          }
           i++;
         }
-        return $('#col-width').val(width).focus();
+        return $('#col-width').text(width);
       }
     });
     $('#menu-content').on('click', function() {
@@ -122,58 +229,56 @@
       elem = $('.col.selected');
       if (elem.length) return $('#content-options').show();
     });
-    $('#col-width').on('change keyup', function(e) {
-      var diff, elem, group, i, s, sw, swnew, w, wnew, _results;
-      if ($('.col.selected').length && e.which === 46) {
-        $('.col.selected').closest('.row').remove();
-        $('#col-settings').hide();
-      }
-      elem = $('.selected.col');
-      w = eval(elem.attr('data-width'));
-      wnew = eval($(this).val());
-      diff = eval(w - wnew);
-      group = [];
-      elem.nextAll().each(function() {
-        return group.push($(this));
-      });
-      elem.prevAll().each(function() {
-        return group.push($(this));
-      });
-      if (diff !== 0) {
-        i = 1;
-        _results = [];
-        while (i <= Math.abs(diff)) {
-          s = '';
-          $(group).each(function(index) {
-            var tw;
-            if (diff < 0) {
-              tw = eval($(this).attr('data-width'));
-              if (tw > 1) {
-                s = $(this);
-                return false;
-              }
-            } else {
-              s = $(this);
-              return false;
-            }
-          });
-          if (s !== '') {
-            sw = eval(s.attr('data-width'));
-            swnew = diff > 0 ? eval(sw + 1) : eval(sw - 1);
-            elem.removeClass('span_' + w).addClass('span_' + wnew).attr('data-width', wnew);
-            s.removeClass('span_' + sw).addClass('span_' + swnew).attr('data-width', swnew);
-          }
-          _results.push(i++);
-        }
-        return _results;
+    $('#row-id').on('keyup', function() {
+      var elem, row, selectedcol;
+      elem = $(this);
+      selectedcol = $('.col.selected');
+      if (selectedcol.length) {
+        row = $('.col.selected').closest('.row');
+        return row.attr('id', elem.val());
       }
     });
-    $('#styles').on('keypress', function() {
+    $('#row-class').on('keyup', function() {
+      var elem, row, selectedcol, x, x1, x2;
+      elem = $(this);
+      selectedcol = $('.col.selected');
+      if (selectedcol.length) {
+        row = $('.col.selected').closest('.row');
+        x1 = row.attr('class').match(/row/g);
+        x2 = row.attr('class').match(/span_\d*/g);
+        x = x1.concat(x2);
+        return row.attr('class', x.toString().replace(/,/g, ' ') + ' ' + elem.val());
+      }
+    });
+    $('#col-id').on('keyup', function() {
+      var elem, selectedcol;
+      elem = $(this);
+      selectedcol = $('.col.selected');
+      if (selectedcol.length) return selectedcol.attr('id', elem.val());
+    });
+    $('#col-class').on('keyup', function() {
+      var elem, selectedcol, x, x1, x2, x3;
+      elem = $(this);
+      selectedcol = $('.col.selected');
+      if (selectedcol.length) {
+        x1 = selectedcol.attr('class').match(/col/g);
+        x2 = selectedcol.attr('class').match(/span_\d*/g);
+        x3 = selectedcol.attr('class').match(/selected/g);
+        x = x1.concat(x2, x3);
+        return selectedcol.attr('class', x.toString().replace(/,/g, ' ') + ' ' + elem.val());
+      }
+    });
+    $('.col-width .sb').on('click', function() {
+      return setTimeout(function() {
+        return updateColWidth($('#col-width').text());
+      }, 10);
+    });
+    $('#styles').on('keyup', function() {
       var style;
       style = $('#user-styles');
       return style.html($('#styles').val());
     });
-    return $('#editor').on('keypress', function() {
+    return $('#editor').on('keyup', function() {
       var elem;
       elem = $('.col.selected');
       if (elem.length) {
